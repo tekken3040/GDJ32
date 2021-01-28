3e6bb1ccfca33bfa91dc5db370c3486bdd3a2808
tekken3040 <tekken3040@naver.com>
2021-01-28 16:19:13 +0900

    asdasd

--- miniProject/js/function.js
+++ miniProject/js/function.js
@@ -1,7 +1,30 @@
+// 지도를 표시할 div
 var container = document.getElementById('map');
+// 초기값
 var options = {
-	center: new kakao.maps.LatLng(33.450701, 126.570667),
-	level: 3
+	center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+	level: 3    // 지도의 확대 레벨
 };
 
+// 지도를 생성합니다.
 var map = new kakao.maps.Map(container, options);
+
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
+let address = '서울시 금천구 가산디지털2로 115';
+
+// 주소-좌표 변환 객체를 생성합니다
+geocoder.addressSearch(address, function(result, status){
+    // 정상적으로 검색이 완료됐으면 
+    if (status === kakao.maps.services.Status.OK) {
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: coords
+        });
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+    }
+});
